b022a321b098512336c090f936d585316cc51811
Bmadz82 <madzhielinda@gmail.com>
2024-09-22 21:47:38 +0200

    updated

--- src/Index.js
+++ src/Index.js
@@ -1,20 +1,26 @@
-document.getElementById('city').addEventListener('change', function() {
+ddocument.getElementById('city').addEventListener('change', function() {
   const city = this.value;
 
-  // Define city timezones
+  // Define timezones
   const timezones = {
-    current: moment.tz.guess(),
-    'Africa/Pretoria': 'Africa/Johannesburg',
-    'Africa/Harare': 'Africa/Harare',
-    'Europe/Paris': 'Europe/Paris'
+      current: moment.tz.guess(),
+      'Africa/Pretoria': 'Africa/Johannesburg',
+      'Africa/Harare': 'Africa/Harare',
+      'Europe/Paris': 'Europe/Paris'
   };
 
   const timezone = timezones[city] || timezones.current;
 
   const now = moment.tz(timezone);
 
+  // Update the display for selected city
   if (city) {
-    document.querySelector(`#${city.split('/')[1] || 'current'} .date`).innerText = now.format('MMMM Do YYYY');
-    document.querySelector(`#${city.split('/')[1] || 'current'} .time`).innerText = now.format('hh:mm:ss A');
+      document.querySelector(`#${city.split('/')[1] || 'current'} .date`).innerText = now.format('MMMM Do YYYY');
+      document.querySelector(`#${city.split('/')[1] || 'current'} .time`).innerText = now.format('hh:mm:ss A');
+  } else {
+      // Clear the date and time if no city is selected
+      document.querySelectorAll('.date, .time').forEach(element => {
+          element.innerText = '';
+      });
   }
 });
